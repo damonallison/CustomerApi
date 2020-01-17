ca4416be1c6746e0a5880f6724ccd290387b0496
Damon Allison <damon@damonallison.com>
2020-01-16 20:20:16 -0600

    Hopefully a better project structure

---
+++ CustomerApi.Tests/CustomerTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CustomerApi.Tests
+{
+    public class CustomerTest1
+    {
+        [Fact]
+        public void ExampleUnitTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
